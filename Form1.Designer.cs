aa8b624562afd41e6c32e9244319d4c8f4f8b022
Hussain Electricwala <hussainelectricwala8@gmail.com>
2020-05-18 01:10:42 +0530

    initial ui design completed

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,25 +33,38 @@
             // 
             // comboBox1
             // 
-            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.Simple;
+            this.comboBox1.AllowDrop = true;
+            this.comboBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(38)))), ((int)(((byte)(38)))));
+            this.comboBox1.DropDownHeight = 500;
+            this.comboBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboBox1.Font = new System.Drawing.Font("Times New Roman", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.ForeColor = System.Drawing.Color.White;
             this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.IntegralHeight = false;
             this.comboBox1.Items.AddRange(new object[] {
             "hussain",
             "zainab",
             "fatema",
             "ali"});
-            this.comboBox1.Location = new System.Drawing.Point(331, 92);
+            this.comboBox1.Location = new System.Drawing.Point(207, 21);
+            this.comboBox1.MaximumSize = new System.Drawing.Size(1080, 0);
+            this.comboBox1.MaxLength = 100;
+            this.comboBox1.MinimumSize = new System.Drawing.Size(100, 0);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 150);
+            this.comboBox1.Size = new System.Drawing.Size(800, 63);
             this.comboBox1.TabIndex = 0;
+            this.comboBox1.TextUpdate += new System.EventHandler(this.comboBox1_TextUpdate);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(1019, 519);
             this.Controls.Add(this.comboBox1);
+            this.Cursor = System.Windows.Forms.Cursors.Cross;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Location = new System.Drawing.Point(500, 500);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
--- Form1.cs
+++ Form1.cs
@@ -15,6 +15,42 @@ namespace Spotlight
         public Form1()
         {
             InitializeComponent();
+            // to set the background transparent 
+            this.BackColor = Color.Blue;
+            this.TransparencyKey = Color.Blue;
+
+
+
+            //giving the style and initial size of the drop down
+            comboBox1.DropDownStyle = ComboBoxStyle.Simple;
+            comboBox1.Size = new Size(720, 100);
+
+            //Initial text in the search
+            comboBox1.Text = "Search";
+            
+            
+        }
+
+        private void comboBox1_TextUpdate(object sender, EventArgs e)
+        {
+
+            String tmp = comboBox1.Text;
+            this.comboBox1.Items.Clear();
+            comboBox1.SelectionStart = 0;
+            comboBox1.SelectionLength = 0;
+            for(int i=0;i<6;i++)
+            comboBox1.Items.Add("HEllo");
+            comboBox1.Select(tmp.Length, 0);
+            comboBox1.Size = new Size(720, 300);
+
+
+
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
