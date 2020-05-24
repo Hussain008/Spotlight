4ff32bb3f29251e6d18b2c3d5a32bebaa0431251
Hussain Electricwala <hussainelectricwala8@gmail.com>
2020-05-24 09:50:02 +0530

    added Trie and TrieNode classes

---
+++ TrieNode.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spotlight
+{
+    //A TrieNode would contain the current character , A list of files and a map of its children .
+    class TrieNode
+    {
+
+        public char character;
+        public ArrayList files;
+        public Dictionary<char, TrieNode> children;
+
+        public TrieNode(char c)
+        {
+            character = c;
+            files = new ArrayList();
+            children = new Dictionary<char, TrieNode>();
+        }
+    }
+}
