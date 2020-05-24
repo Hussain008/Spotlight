9b879b3835dd21681be0b7a607da184593626fd1
Hussain Electricwala <hussainelectricwala8@gmail.com>
2020-05-24 09:43:50 +0530

    created the textfile with all the folder and files

--- all_files_list.py
+++ all_files_list.py
@@ -1,25 +1,34 @@
 import os
 import glob
 
+all_files = []
+
 def walking(path):
-        global count
+        global all_files
 
         #program fails if the size of the path is greater than 250 characters / cannot open it then
         files_in_cur_dir = [x for x in glob.glob(path) if len(x)<=250 ]
 
-        #sort the files in the order of recently used 
-        sorted_by_use =  sorted( files_in_cur_dir ,key=os.path.getctime)
+        for file in files_in_cur_dir : 
 
-
-        for file in sorted_by_use : 
-            
             #if current file is a directory , then recursively expand
             if(os.path.isdir(file)):
                 walking(file+'\*')
+            
+            all_files.append(file)
 
-            with open("C:\\Users\\gunny\\source\\repos\\Spotlight\\allFiles.txt", "a", encoding="utf-8") as filewrite:
-                filewrite.write(file+" "+str(count)+"\n")
-                count+=1
+            
+
+def write_to_file():
+    global all_files
+    global count
+    
+    sorted_by_use =  sorted( all_files ,key=os.path.getctime)
+
+    for file in sorted_by_use : 
+        with open("C:\\Users\\gunny\\source\\repos\\Spotlight\\allFilesCopy.txt", "a", encoding="utf-8") as filewrite:
+                    filewrite.write(file+" "+str(count)+"\n")
+                    count+=1
 
 Apps = 'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\*'
 Documents = 'C:\\Users\\gunny\\Documents\\*'
@@ -31,4 +40,6 @@ walking(Apps)
 walking(Desktop)
 walking(Documents)
 walking(Downloads)
+
+write_to_file()
 
